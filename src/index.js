68e62bf8b39b0569fbf8a9e9c11986fe9edf5a8c
Joao Bosco <diasnetojoaobosco@gmail.com>
2023-06-26 15:55:31 -0300

    inicio

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+
+
+const Stack = createStackNavigator();
+
+export default function RootNavigation() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
