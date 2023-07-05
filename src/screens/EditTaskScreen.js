34c7a37e6d697a02e937ab925bf1373ad942164a
isabella <isabellaschmitz00@gmail.com>
2023-07-05 13:50:59 -0300

    aa

--- src/screens/EditTaskScreen.js
+++ src/screens/EditTaskScreen.js
@@ -1,14 +1,47 @@
-import { View } from "react-native";
-import { Paragraph, Text } from "react-native-paper";
-import styles from "../utils/styles";
 
-export default function EditTaskScreen({ navigation }) {
+import React, { useState, useEffect } from 'react';
+import { View, TextInput, Button} from 'react-native';
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
+import { db } from '../config/firebase';
+
+const EditTaskScreen = ({ route, navigation }) => {
+  const { taskId } = route.params;
+  const [task, setTask] = useState('');
+
+  useEffect(() => {
+    const fetchTask = async () => {
+      const taskDocRef = doc(db, 'tasks', taskId);
+      const taskDocSnapshot = await getDoc(taskDocRef);
+      if (taskDocSnapshot.exists()) {
+        const taskData = taskDocSnapshot.data();
+        setTask(taskData.nome);
+      }
+    };
+
+    fetchTask();
+  }, [taskId]);
+
+  const handleUpdateTask = async () => {
+    const taskDocRef = doc(db, 'tasks', taskId);
+    await updateDoc(taskDocRef, { nome: task });
+
+    // Volta para a página anterior (Home)
+    navigation.goBack();
+  };
+
   return (
     <View style={styles.container}>
-      <Paragraph style={styles.title}>Edit Screen</Paragraph>
-      <Text style={styles.subtitle}>
-        Aqui você pode editar as tarefas que você criou.
-      </Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Digite uma tarefa"
+        value={task}
+        onChangeText={text => setTask(text)}
+      />
+      {/* <Button title="Atualizar" onPress={handleUpdateTask} /> */}
     </View>
   );
-}
+};
+
+
+
+export default EditTaskScreen;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,9 @@
 import { ScrollView, View } from "react-native";
-import { Paragraph, Text } from "react-native-paper";
+import { Paragraph, Text,  Button } from "react-native-paper";
 import styles from "../utils/styles";
 import React, { useState, useEffect } from "react";
-import { collection, query, onSnapshot } from "firebase/firestore";
+// import { deleteDoc } from "firebase/firestore";
+import { collection, doc, onSnapshot } from "firebase/firestore"; //query talvez
 import { db } from "../config/firebase";
 
 const HomeScreen = () => {
@@ -13,10 +14,27 @@ const HomeScreen = () => {
     const unsubscribe = onSnapshot(tasksCollectionRef, (snapshot) => {
       const taskList = snapshot.docs.map((doc) => doc.data().nome);
       setTasks(taskList);
+      // const taskList = snapshot.docs.map((doc) => doc.data());
+      const tasksListsWithIds = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      setTasks(tasksListsWithIds);
     });
 
     return () => unsubscribe();
   }, []);
+
+  const handleEditTask = (taskId) => {
+    console.log(taskId);
+    navigation.navigate("EditTask", { taskId });
+  };
+
+  const handleDeleteTask = async (taskId) => {
+    const taskDocRef = doc(db, "tasks", taskId);
+    await deleteDoc(taskDocRef);
+  };
 
   return (
     <View style={styles.container}>
@@ -24,7 +42,7 @@ const HomeScreen = () => {
       <Paragraph style={styles.subtitle}>
         Aqui você pode ver as suas tarefas!!!
       </Paragraph>
-      <ScrollView>
+      {/* <ScrollView>
         <View style={styles.content_tasks}>
           {tasks.map((task, index) => (
             <Text style={styles.tasks} key={index}>
@@ -32,7 +50,17 @@ const HomeScreen = () => {
             </Text>
           ))}
         </View>
-      </ScrollView>
+      </ScrollView> */}
+      {tasks.map((task, index) => (
+        <View key={index}>
+          {console.log(task)}
+          <Text key={index}>{task.nome}</Text>
+          <View>
+            <Button onPress={() => handleEditTask(task.id)}> Editar </Button>
+            <Button onPress={() => handleDeleteTask(task.id)}> Excluir</Button>
+          </View>
+        </View>
+      ))}
     </View>
   );
 };
